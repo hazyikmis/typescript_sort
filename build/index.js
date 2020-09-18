89c467567c898c6c76d9c228f5d17eb278b82630
Halil Azyikmis <hazyikmis@yahoo.com>
2020-09-18 18:09:11 +0200

    first bubble sort implementation for numbers

--- build/index.js
+++ build/index.js
@@ -1,6 +1,33 @@
 "use strict";
-console.log('hi there');
-var logSomething = function () {
-    console.log('something');
-};
-logSomething();
+// class Sorter {
+//   //collection is tha data we want to sort
+//   collection: number[];
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+var Sorter = /** @class */ (function () {
+    //the method below 100% equivalent with the method above section commented!
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    //implementation of bubble sort
+    //void, because nothing returned, all done inside is this.collection will be sorted
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        var leftHand;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 11, 0, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-console.log('hi there');
+// class Sorter {
+//   //collection is tha data we want to sort
+//   collection: number[];
 
-const logSomething = () => {
-  console.log('something');
-};
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
 
-logSomething();
+class Sorter {
+  //the method below 100% equivalent with the method above section commented!
+  constructor(public collection: number[]) {}
+
+  //implementation of bubble sort
+  //void, because nothing returned, all done inside is this.collection will be sorted
+  sort(): void {
+    const { length } = this.collection;
+
+    let leftHand;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 11, 0, 0]);
+sorter.sort();
+console.log(sorter.collection);
